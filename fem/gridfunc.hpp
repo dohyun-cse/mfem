5557dd51d2ea29a53bfd908dd595cc28feb41742
Dohyun Kim <dohyun_kim@brown.edu>
2023-07-18 11:43:42 -0400

    prolong

--- fem/gridfunc.hpp
+++ fem/gridfunc.hpp
@@ -854,6 +854,8 @@ public:
 GridFunction *Extrude1DGridFunction(Mesh *mesh, Mesh *mesh2d,
                                     GridFunction *sol, const int ny);
 
+GridFunction* ProlongToMaxOrder(const GridFunction *x, const int fieldtype=1);
+
 } // namespace mfem
 
 #endif
