d18baf1cab19002bc2459780a2abcc8202afc5d0
Dohyun Kim <dohyun_kim@brown.edu>
2026-04-02 21:56:35 -0400

    block?

--- linalg/blockmatrix.cpp
+++ linalg/blockmatrix.cpp
@@ -561,9 +561,41 @@ SparseMatrix * BlockMatrix::CreateMonolithic() const
 {
    int nnz = NumNonZeroElems();
 
-   int * i_amono = Memory<int>(row_offsets[nRowBlocks]+2);
-   int * j_amono = Memory<int>(nnz);
-   real_t * data = Memory<real_t>(nnz);
+   int * i_amono = nullptr;
+   int * j_amono = nullptr;
+   real_t * data = nullptr;
+   MemoryType h_mt, d_mt;
+   for (int i=0; i<nRowBlocks + 1; i++)
+   {
+      for (int j=0; j<nColBlocks + 1; j++)
+      {
+         if (Aij(i,j) != NULL)
+         {
+            MemoryType h_mt_curr = Aij(i,j)->GetMemoryI().GetHostMemoryType();
+            MemoryType d_mt_curr = Aij(i,j)->GetMemoryI().GetDeviceMemoryType();
+            if (i_amono == nullptr)
+            {
+               h_mt = h_mt_curr;
+               d_mt = d_mt_curr;
+               i_amono = Memory<int>(row_offsets[nRowBlocks]+2, h_mt, d_mt);
+               j_amono = Memory<int>(nnz, h_mt, d_mt);
+               data = Memory<real_t>(nnz, h_mt, d_mt);
+            }
+            else
+            {
+               MFEM_VERIFY(h_mt == h_mt_curr && d_mt == d_mt_curr,
+                           "BlockMatrix::CreateMonolithic: Inconsistent memory types");
+            }
+            break;
+         }
+      }
+   }
+   Array2D<const int*> Aij_I(nRowBlocks,nColBlocks);
+   Aij_I = (const int*)NULL;
+   Array2D<const int*> Aij_J(nRowBlocks,nColBlocks);
+   Aij_J = (const int*)NULL;
+   Array2D<const real_t*> Aij_Data(nRowBlocks,nColBlocks);
+   Aij_Data = (const real_t*)NULL;
 
    for (int i = 0; i < row_offsets[nRowBlocks]+2; i++)
    {
@@ -581,8 +613,13 @@ SparseMatrix * BlockMatrix::CreateMonolithic() const
          for (int jblock = 0; jblock < nColBlocks; ++jblock)
          {
             if (Aij(iblock,jblock) != NULL)
-               ind += Aij(iblock, jblock)->GetI()[local_row+1]
-                      - Aij(iblock, jblock)->GetI()[local_row];
+            {
+               Aij_I(iblock, jblock) = Aij(iblock, jblock)->ReadI();
+               Aij_J(iblock, jblock) = Aij(iblock, jblock)->ReadJ();
+               Aij_Data(iblock, jblock) = Aij(iblock, jblock)->ReadData();
+               ind += Aij_I(iblock, jblock)[local_row+1]
+                      - Aij_I(iblock, jblock)[local_row];
+            }
          }
          i_amono_construction[irow+1] = ind;
       }
@@ -596,9 +633,9 @@ SparseMatrix * BlockMatrix::CreateMonolithic() const
          if (Aij(iblock,jblock) != NULL)
          {
             int nrow = row_offsets[iblock+1]-row_offsets[iblock];
-            int * i_aij = Aij(iblock, jblock)->GetI();
-            int * j_aij = Aij(iblock, jblock)->GetJ();
-            real_t * data_aij = Aij(iblock, jblock)->GetData();
+            const int * i_aij = Aij_I(iblock, jblock);
+            const int * j_aij = Aij_J(iblock, jblock);
+            const real_t * data_aij = Aij_Data(iblock, jblock);
             int *i_it = i_amono_construction+row_offsets[iblock];
 
             int loc_start_index = 0;
@@ -606,7 +643,7 @@ SparseMatrix * BlockMatrix::CreateMonolithic() const
             int glob_start_index = 0;
 
             int shift(col_offsets[jblock]);
-            for (int * i_it_aij(i_aij+1); i_it_aij != i_aij+nrow+1; ++i_it_aij)
+            for (const int * i_it_aij = i_aij+1; i_it_aij != i_aij+nrow+1; ++i_it_aij)
             {
                glob_start_index = *i_it;
 
