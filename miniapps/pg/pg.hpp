adc81cdb0d52e76c24bf06ccf36e0b2fc453317a
Dohyun Kim <dohyun_kim@brown.edu>
2026-04-02 22:19:05 -0400

    serial keep data

--- miniapps/pg/pg.hpp
+++ miniapps/pg/pg.hpp
@@ -21,6 +21,7 @@ private:
    std::unique_ptr<BilinearForm> dualhess;
    std::unique_ptr<LinearForm> dualgrad;
    mutable std::unique_ptr<SparseMatrix> pg_op;
+   mutable SparseMatrix dualH_serial;
    mutable Array<int> latent_ess_tdof;
    Array<int> offsets;
    bool parallel = false;
