de3ca35f9e5006999d2e9b35a0d75c7094dbc559
Dohyun Kim <dohyun_kim@brown.edu>
2026-04-06 19:46:17 -0400

    read and device

--- linalg/cudss.cpp
+++ linalg/cudss.cpp
@@ -174,10 +174,10 @@ void CuDSSSolver::SetMatrix(const HypreParMatrix &op)
 {
    bool cuDSSObjectInitialized = (Ac != nullptr);
 
+   op.HypreRead();
    const HypreParMatrix *A = dynamic_cast<const HypreParMatrix *>(&op);
    hypre_ParCSRMatrix *parcsr_op = *A;
-   hypre_CSRMatrix *csr_op = hypre_MergeDiagAndOffd(parcsr_op);
-   op.HypreRead();
+   hypre_CSRMatrix *csr_op = hypre_MergeDiagAndOffdDevice(parcsr_op);
 #if MFEM_HYPRE_VERSION >= 21600
    hypre_CSRMatrixBigJtoJ(csr_op);
 #endif
